3fb0380ce6e908eea4866bdcca58de5ffc98b5f1
Hongsuk Kang <hongsuk.kang@nist.gov>
2019-11-05 16:53:15 -0500

    Add missing FixBDOMP destructor

--- src/BROWNIAN/fix_bd_omp.h
+++ src/BROWNIAN/fix_bd_omp.h
@@ -27,7 +27,7 @@ namespace LAMMPS_NS {
 class FixBDOMP : public FixBD {
  public:
   FixBDOMP(class LAMMPS *, int, char **);
-  virtual ~FixBDOMP();
+  virtual ~FixBDOMP() { }
   virtual void initial_integrate(int);
   virtual void final_integrate();
 };
